aa075f0d3719f22794ef58b4a873381b97f2b555
Chien-pin Wang <Wang.ChienPin@gmail.com>
2017-01-03 13:59:30 +0800

    Show one buoy report at a time
    - Select one buoy station at a time
    - Report the buoy station
    - Confirm if select other buoy station or quit

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ Buoy is a command line application that reports and analyze data collected by bu
 
 ## To-do's
 
+- [ ] [Issue #1](https://github.com/Chien-pinWang/Buoy/issues/1): Select and report one buoy at a time
 
 ## Contributing
 
--- index.php
+++ index.php
@@ -16,36 +16,61 @@ $buoyLog = new Logger('buoyLog');
 $buoyLog->pushHandler(new StreamHandler(LOG_FILE));
 
 // Invoke user selections of interested buoy stations
-$allBuoys = [
-    'C6AH2' => '富貴角浮標',
-     '46694A' => '龍洞浮標',
-     '46708A' => '龜山島浮標',
-     '46761F' => '成功浮標',
-     '46759A' => '鵝鑾鼻浮標',
-     '46714D' => '小琉球浮標',
-     'COMC08' => '彌陀浮標'
-];
-$CLI = new CLImate;
-$CLI->clear();
-$input = $CLI->checkboxes('選擇要分析的浮標資料: ', $allBuoys);
-$buoys = $input->prompt();
-if (count($buoys) > 0) {
-    $CLI->yellow('正在取得浮標資料中, 請稍候...');
-    $CLI->border('~', 77);
+
+function getUserInput(CLImate $CLI) {
+/**
+    $allBuoys = [
+        'C6AH2' => '富貴角浮標',
+        '46694A' => '龍洞浮標',
+        '46708A' => '龜山島浮標',
+        '46761F' => '成功浮標',
+        '46759A' => '鵝鑾鼻浮標',
+        '46714D' => '小琉球浮標',
+        'COMC08' => '彌陀浮標'
+    ];
+ */
+    $allBuoys = [
+        '富貴角浮標' => 'C6AH2',
+        '龍洞浮標' => '46694A',
+        '龜山島浮標' => '46708A',
+        '成功浮標' => '46761F',
+        '鵝鑾鼻浮標' => '46759A',
+        '小琉球浮標' => '46714D',
+        '彌陀浮標' => 'COMC08',
+        '[結束程式]' => '__END__'
+    ];
+    $CLI->clear();
+    $input = $CLI->radio('選擇要分析的浮標資料: ', array_keys($allBuoys));
+    $buoyName = $input->prompt();
+    $buoyID = $allBuoys[$buoyName];
+    if ($buoyID == '__END__') {
+        exit();
+    } else {
+        $CLI->yellow('正在取得浮標資料中, 請稍候...');
+        $CLI->border('~', 77);
+        return $buoyID;
+    }
 }
 
+$CLI = new CLImate;
+
 // Populate buoy data by iterating through all selected stations
-for ($i = 0; $i < count($buoys); $i++) {
+// for ($i = 0; $i < count($buoys); $i++) {
+while ($buoyID = getUserInput($CLI)) {
     try {
-        $buoy = new Buoy($buoys[$i]);
+        $buoy = new Buoy($buoyID);
         $CLI->lightGreen($buoy->getBuoyName());
         // 1.2.0: Use getBuoyReportArray(LATEST_X_HOURS) to get report in 
         // array so the report can be displayed by $CLI->table formation
         // $CLI->out($buoy->getBuoyReport(LATEST_X_HOURS));
         $CLI->table($buoy->getBuoyReportArray(LATEST_X_HOURS, VERBOSE_LEVEL));
-        $buoyLog->info('Successfully reported buoy info for ' . $buoy->getBuoyName(), array('buoyID' => $buoys[$i]));
+        $buoyLog->info('Successfully reported buoy info for ' . $buoy->getBuoyName(), array('buoyID' => $buoyID));
+        $confirm = $CLI->yellow()->confirm('選擇其他浮標?');
+        if (!$confirm->confirmed()) {
+            break;
+        }
     } catch (\Exception $e) {
-        $CLI->red('錯誤: 無法取得浮標編號 ' . $buoys[$i] . ' 資料!');
+        $CLI->red('錯誤: 無法取得浮標編號 ' . $buoyID . ' 資料!');
         $buoyLog->error('Failed to connect to buoy data source. Error message: ' . $e->getMessage(), array('Script' => $e->getFile(), 'Line No.' => $e->getLine()));
     }
 }
